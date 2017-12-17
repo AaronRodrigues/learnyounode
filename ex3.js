ee0f5ab7e29162797a1df1c65b946fef6836f93c
AaronRodrigues <aaronrod@gmail.com>
2017-12-17 19:09:58 +0000

    Adds ex4

--- ex3.js
+++ ex3.js
@@ -1,9 +1,31 @@
-var fs = require('fs');
-
+var fs = require('fs');    
+    
 var buf = fs.readFileSync(process.argv[2]);
-//console.log("File loaded");
-
+  
 var str = buf.toString();
 str = str.split("\n");
 str = str.length - 1 ;
 console.log(str);
+
+
+
+
+
+// Official Answer
+  // var fs = require('fs')
+
+  //  var contents = fs.readFileSync(process.argv[2])
+
+  //var lines = contents.toString().split('\n').length - 1
+
+  //  console.log(lines)
+
+    
+
+    // note you can avoid the .toString() by passing 'utf8' as the
+
+    // second argument to readFileSync, then you'll get a String!
+
+    //
+
+    // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
---
+++ ex4.js
@@ -0,0 +1,43 @@
+vae fs = require('fs');    
+
+function count(){    
+  fs.readFile(process.argv[2], function finishedReading(error, buf){
+    if (error) return console.error(error);    
+    var str = buf.toString();
+    str = str.split("\n");
+    str = str.length - 1 ;
+    console.log(str);
+  });
+}
+
+count();
+
+//Here's the official solution in case you want to compare notes:
+
+
+
+//─────────────────────────────────────────────────────────────────────────────
+
+
+
+//    var fs = require('fs')
+
+//    var file = process.argv[2]
+
+    
+
+//    fs.readFile(file, function (err, contents) {
+
+//      if (err) {
+
+//        return console.log(err)
+
+//      }
+//      // fs.readFile(file, 'utf8', callback) can also be used
+
+//      var lines = contents.toString().split('\n').length - 1
+
+//      console.log(lines)
+
+//    })
+//
