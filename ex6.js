9e0e8dd1757fa7f83eb5924d0110e23b8ae83eb9
Aaron Rodrigues <aaronrodrigues@PEG-MAC-008391.local>
2017-12-29 13:19:31 +0000

    Second Try

--- ex6.js
+++ ex6.js
@@ -1,14 +1,60 @@
- var mymodule = require('./mymodule.js')
+var mymodule = require('./mymodule.js');
 
- var folder = process.argv[2];
- var ext =  process.argv[3];
-mymodule(folder,ext,callback);
+var folder = process.argv[2];
+var ext = process.argv[3];
 
-function callback(err,data)
-{
-  if(err){console.log(err)}
+mymodule(folder, ext, callback);
+
+function callback(err, data){
+  if (err) return callback(err)
   data.forEach(element => {
     console.log(element);
   });
-
 }
+
+/*
+# PASS Your solution to MAKE IT MODULAR passed!
+
+ Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+ _/usr/local/lib/node_modules/learnyounode/exercises/make_it_modular/soluti
+ on/solution.js_ :
+
+
+    var filterFn = require('./solution_filter.js')
+    var dir = process.argv[2]
+    var filterStr = process.argv[3]
+
+    filterFn(dir, filterStr, function (err, list) {
+      if (err) {
+        return console.error('There was an error:', err)
+      }
+
+      list.forEach(function (file) {
+        console.log(file)
+      })
+    })
+
+─────────────────────────────────────────────────────────────────────────────
+ _/usr/local/lib/node_modules/learnyounode/exercises/make_it_modular/soluti
+ on/solution_filter.js_ :
+
+
+    var fs = require('fs')
+    var path = require('path')
+
+    module.exports = function (dir, filterStr, callback) {
+      fs.readdir(dir, function (err, list) {
+        if (err) {
+          return callback(err)
+        }
+
+        list = list.filter(function (file) {
+          return path.extname(file) === '.' + filterStr
+        })
+
+        callback(null, list)
+      })
+    }
+*/   
--- mymodule.js
+++ mymodule.js
@@ -1,20 +1,16 @@
+module.exports = function(folder, ext, callback){
 
-module.exports = function(folder, ext, callback) {
- var fs = require('fs')
- var path = require('path')
+var fs = require('fs')
+var path = require('path')
+var data = [];
+  fs.readdir(folder, function (err, files) {
+    if (err) return callback(err)
+    files.forEach(function (file) {
+      if (path.extname(file).replace(".", "") === ext) {
+        data.push(file);
+      }
+    })
+    callback(null, data)
+  });
 
-
- var data=[];
-fs.readdir(folder,  function (err, files) {  
-      if (err) return callback(err);
-     
-        files.forEach(function (file) {
-        if (path.extname(file).replace(".","") === ext) {
-          data.push(file);
-        }
-      })
-      callback(null, data);
-    });
-
-}
-
+};
